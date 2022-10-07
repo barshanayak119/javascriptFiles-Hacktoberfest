564b6d216e8f220f9f59956566e1706d1bd57f1f
rubybano <56728725+rubybano@users.noreply.github.com>
2022-10-07 19:54:25 +0530

    Update substract_num_JS.js

--- substract_num_JS.js
+++ substract_num_JS.js
@@ -4,3 +4,15 @@ let y = 5
 let z = x-y
 
 console.log(z)
+
+console.log(5 - 3);
+// expected output: 2
+
+console.log(3.5 - 5);
+// expected output: -1.5
+
+console.log(5 - 'hello');
+// expected output: NaN
+
+console.log(5 - true);
+// expected output: 4
