c1cd85a615df225c8947a537617d303908eff943
rubybano <56728725+rubybano@users.noreply.github.com>
2022-10-07 19:44:49 +0530

    Update function_to_check_for_palindrome.js
    Added the java code to find palindrome or not.

--- function_to_check_for_palindrome.js
+++ function_to_check_for_palindrome.js
@@ -1,24 +1,22 @@
-function check_palindrome(string) {
-    let length = string.length - 1;
-    for(let i = 0; i<j/2; i++){
-        if(str[i] !== str[length-i]){
-           return false;
-        }
-    }
-    return true;
-}
-
-let test_case_1 = "abcdef";
-let test_case_2 = "cabdbac";
-
-if(check_palindrome(test_case_1)){
-    console.log("test_case_1 is palindrome");
-}else{
-    console.log("test_case_1 is not palindrome");
-}
-
-if(check_palindrome(test_case_2)){
-    console.log("test_case_2 is palindrome");
-}else{
-    console.log("test_case_2 is not palindrome");
+import java.util.Scanner;
+class Palindrome
+{
+	public static void main(String arg[])	
+	{
+	    int num,t,s,rem;
+             	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter any number ");
+                   num=sc.nextInt();
+	    t=num;
+	    for(s=0;num>0;num/=10)
+	    {
+	    rem=num%10;
+	    s=(s*10)+rem;
+	    }
+	   if(s==t)
+		System.out.println(t+" is a palindrome number ");
+                  else
+		System.out.println(t+" is not a palindrome number ");
+                  
+	}
 }
