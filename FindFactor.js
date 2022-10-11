c632bda086fa33353de0743a7ab96cbd57b379bc
LishaNayak <115545659+LishaNayak@users.noreply.github.com>
2022-10-11 18:44:42 +0530

    created a program to find factor

---
+++ FindFactor.js
@@ -0,0 +1,12 @@
+const num = prompt('Enter a positive number: ');
+
+console.log(`The factors of ${num} is:`);
+
+// looping through 1 to num
+for(let i = 1; i <= num; i++) {
+
+    // check if number is a factor
+    if(num % i == 0) {
+        console.log(i);
+    }
+}
